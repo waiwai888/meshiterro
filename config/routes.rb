abf8d3e64256b64c633544da9b9a62a871ffba98
waiwai888 <ec2-user@ip-172-31-50-237.ec2.internal>
2021-06-14 11:37:32 +0000

    [start]4-5

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
     resources :post_comments, only: [:create, :destroy]
       resource :favorites, only: [:create, :destroy]
   end
+  resources :users, only: [:show]
 end
