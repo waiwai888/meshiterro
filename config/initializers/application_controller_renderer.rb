5eb4d8e8bd9e98417cbcff8f76317f69488966d8
waiwai888 <ec2-user@ip-172-31-50-237.ec2.internal>
2021-06-14 06:29:40 +0000

    [finish]4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -1,4 +1,4 @@
-# Be sure to restart your server when you modify this file.
+Refile.secret_key = '6968fe38b213fc99b7e5bb36fac5d62dd286ed878e9cce6ae283ad11927c15d93b05e3dc17990a1acf940a7eca5025939f557c90d3208ee526084a4d5b5a5f6d'# Be sure to restart your server when you modify this file.
 
 # ActiveSupport::Reloader.to_prepare do
 #   ApplicationController.renderer.defaults.merge!(
